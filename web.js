7be260be5a15aec7ffee8c710deb72045b70487c
Ryan LeTulle <bayousoft@gmail.com>
2013-07-08 00:41:08 +0000

    TS Including index.html file

--- web.js
+++ web.js
@@ -8,11 +8,11 @@ app.get('/', function(request, response) {
   var fs = require('fs');
 
   // file is included here:
-  eval(fs.readFileSync('index.html')+'');
+  response = eval(fs.readFileSync('index.html')+'');
   response.send(response);
 });
 
-var port = process.env.PORT || 5000;
+Var port = process.env.PORT || 5000;
 app.listen(port, function() {
-  console.log("Listening on " + port);
+  console.log("Listening On " + port);
 });
