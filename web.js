2af9cb66c03d6ee355c426f505145f78a5b74115
Ryan LeTulle <bayousoft@gmail.com>
2013-07-08 01:09:57 +0000

    TS Including index.html file

--- web.js
+++ web.js
@@ -1,18 +1,12 @@
-//var fs = require('fs');
 var express = require('express');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  
-  // file is included here:
-  //includefile = fs.readFileSync('index.html').toString;
-  includefile = "test";
-  response.send(includefile);
-
+  response.send('Hello World!');
 });
 
-Var port = process.env.PORT || 5000;
+var port = process.env.PORT || 5000;
 app.listen(port, function() {
-  console.log("Listening On " + port);
+  console.log("Listening on " + port);
 });
