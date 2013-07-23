1e4cb60d920c15694f25328ab953239bee3dc70c
Ryan P. LeTulle <bayousoft@gmail.com>
2013-07-23 18:47:49 -0500

    Added images title tagline

--- web.js
+++ web.js
@@ -9,6 +9,7 @@ app.get('/', function(request, response) {
 });
 
 var port = process.env.PORT || 8080;
+app.use(express.static(__dirname + '/public'));
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
